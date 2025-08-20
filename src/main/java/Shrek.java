fdb2171a909a8481c598beb593f949847168a186
jj55j7 <e1355127@u.nus.edu>
2025-08-20 23:01:58 +0800

    Add chatbot Shrek with greeting & exit

---
+++ src/main/java/Shrek.java
@@ -0,0 +1,14 @@
+public class Shrek {
+    public static void main(String[] args) {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(" Hello I'm Shrek!");
+        System.out.println(" Welcome to my Swamp!");
+        System.out.println(" What can I do for you?");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        System.out.println(" Bye! I'm going to find Princess Fiona :)");
+        System.out.println(" See ya later");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    }
+}
