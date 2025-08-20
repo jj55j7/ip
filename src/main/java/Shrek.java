9204fc235b19f75c4eee1b69b9cc818e79419722
jj55j7 <e1355127@u.nus.edu>
2025-08-21 00:30:08 +0800

    Add echo functionality and bye command

--- src/main/java/Shrek.java
+++ src/main/java/Shrek.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Shrek {
     public static void main(String[] args) {
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println(" Hello I'm Shrek!");
-        System.out.println(" Welcome to my Swamp!");
-        System.out.println(" What can I do for you?");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("      Hello I'm Shrek!");
+        System.out.println("      Welcome to my Swamp!");
+        System.out.println("      What can I do for you?");
+        System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-        System.out.println(" Bye! I'm going to find Princess Fiona :)");
-        System.out.println(" See ya later");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Scanner myObj = new Scanner(System.in);
 
+        while (true) {
+            String input = myObj.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("      Bye! I'm going to find Princess Fiona :)");
+                System.out.println("      See ya later");
+                System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                break;
+            }
+
+            System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("      " + input);
+            System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+        myObj.close();
     }
 }
+
