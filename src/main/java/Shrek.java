325a3d40225713e6a203960c1a739e87ded864d8
jj55j7 <e1355127@u.nus.edu>
2025-08-21 11:04:58 +0800

    Add mark and unmark functionality

--- src/main/java/Shrek.java
+++ src/main/java/Shrek.java
@@ -10,25 +10,47 @@ public class Shrek {
         System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
         Scanner myObj = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         while (true) {
             String input = myObj.nextLine();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
 
-            if (input.equalsIgnoreCase("bye")) {
+            if (command.equalsIgnoreCase("bye")) {
                 System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 System.out.println("      Bye! I'm going to find Princess Fiona :)");
                 System.out.println("      See ya later");
                 System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 break;
-            } else if (input.equalsIgnoreCase("list")) {
+            } else if (command.equalsIgnoreCase("list")) {
                 System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("      " + (i + 1) + ": " + tasks.get(i));
+                    Task t = tasks.get(i);
+                    System.out.println("      " + (i + 1) + ":[" +  t.getStatusIcon() + "] " + t.description);
                 }
                 System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            } else if (command.equalsIgnoreCase("mark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                Task t = tasks.get(index);
+                t.markAsDone();
+                System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("      Awesome! One layer of onion(task) has been removed");
+                System.out.println("      (marked done)");
+                System.out.println("       " + "[" +  t.getStatusIcon() + "] " + t.description);
+                System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            } else if (command.equalsIgnoreCase("unmark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                Task t = tasks.get(index);
+                t.markAsNotDone();
+                System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("      Awesome! One layer of onion(task) has been added");
+                System.out.println("      (marked as not done yet)");
+                System.out.println("       " + "[" +  t.getStatusIcon() + "] " + t.description);
+                System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             } else {
-                tasks.add(input);
+                Task t = new Task(input);
+                tasks.add(t);
                 System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 System.out.println("      added: " + input);
                 System.out.println("     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
